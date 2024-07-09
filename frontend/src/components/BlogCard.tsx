67f23b64a2ee9c580c1113d2f448edb7bdcc6817
sachin-malani <sachinmalani32@gmail.com>
2024-07-09 21:23:16 +0530

    Blog Card

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,37 @@
+interface BlogCardProps {
+  authorName: string;
+  title: string;
+  content: string;
+  publishedOn: string;
+}
+
+const BlogCard = ({
+  authorName,
+  title,
+  content,
+  publishedOn,
+}: BlogCardProps) => {
+  return (
+    <div className="border-b-2 border-slate-200 p-4">
+      <div className="flex gap-2">
+        <div className="flex flex-col justify-center"><Avatar name={authorName} /></div>
+        <div className="font-extralight text-sm flex flex-col justify-center">{authorName}</div>
+        <div className="flex flex-col justify-center text-xs">&#9679;</div>
+        <div className="font-thin text-slate-500 text-sm flex flex-col justify-center">{publishedOn}</div>
+      </div>
+      <div className="text-xl font-semibold pt-2    ">{title}</div>
+      <div className="text-md font-extralight">{content.slice(0, 100) + "..."}</div>
+      <div className="text-slate-500 text-sm font-thin pt-4">{`${Math.ceil(content.length / 100)} min read`}</div>
+    </div>
+  );
+};
+
+export default BlogCard;
+
+function Avatar({ name }: { name: string }) {
+  return (
+    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-600 rounded-full">
+      <span className="font-medium text-xs text-white">{name[0]}</span>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,30 @@
+import BlogCard from "../components/BlogCard";
+
+const Blogs = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col justify-center max-w-xl">
+        <BlogCard
+          authorName="Sachin Jain"
+          title="How an Ugly Single-Page Website makes $5,000 a Month with Affiliate Marketing"
+          content="content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty"
+          publishedOn="10th July 2024"
+        />
+        <BlogCard
+          authorName="Sachin Jain"
+          title="How an Ugly Single-Page Website makes $5,000 a Month with Affiliate Marketing"
+          content="content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty"
+          publishedOn="10th July 2024"
+        />
+        <BlogCard
+          authorName="Sachin Jain"
+          title="How an Ugly Single-Page Website makes $5,000 a Month with Affiliate Marketing"
+          content="content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty, content of the blog which is not good currently but will work on it and alter it to make it look pretty"
+          publishedOn="10th July 2024"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
