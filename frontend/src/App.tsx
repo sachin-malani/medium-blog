c04ba62f114dfd2dcd8d52c31a7419a7b0f76db1
sachin-malani <sachinmalani32@gmail.com>
2024-07-11 13:38:46 +0530

    Protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Blog from "./pages/Blog";
@@ -12,13 +18,21 @@ function App() {
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/publish" element={<Publish />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
   );
 }
+
+const PrivateRoutes = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+};
 
 export default App;
